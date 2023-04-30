8ffb2e0f99b316ae876f0a47ecabd018d0f3439e
Shahaj <gshahaj@gmail.com>
2023-04-30 23:49:52 +0600

    Change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-v15'`, () => {
+  it(`should have as title 'angular-v15-features'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-v15');
+    expect(app.title).toEqual('angular-v15-features');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-v15 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-v15-features app is running!'
+    );
   });
 });
